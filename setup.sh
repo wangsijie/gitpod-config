be943cd68085c750aa2dafbfdf26d3cfeca18982
Wang Sijie <wangsijie94@gmail.com>
2022-09-04 21:16:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-[[ ! -z $GITCONFIG  ]] && \
-echo $GITCONFIG | base64 -d > ~/.gitconfig && \
+if [[ -z "${GITCONFIG_SILVERHAND}" ]]; then
+  echo $GITCONFIG_SILVERHAND | base64 -d > ~/.gitconfig && \
+else
+  echo $GITCONFIG | base64 -d > ~/.gitconfig && \
+fi
 chmod 644 ~/.gitconfig
 
 mkdir -p ~/.ssh
@@ -10,3 +13,16 @@ chmod 644 ~/.ssh/id_rsa.pub
 [[ ! -z $SSH_PRIVATE_KEY  ]] && \
 echo $SSH_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa && \
 chmod 600 ~/.ssh/id_rsa
+
+[[ ! -z $GPG_PUBLIC  ]] && \
+echo $GPG_PUBLIC > public.key && \
+gpg --import public.key
+[[ ! -z $GPG_PRIVATE  ]] && \
+echo $GPG_PRIVATE > private.key && \
+gpg --import private.key
+[[ ! -z $GPG_PUBLIC_SILVERHAND  ]] && \
+echo $GPG_PUBLIC_SILVERHAND > public.key && \
+gpg --import public.key
+[[ ! -z $GPG_PRIVATE_SILVERHAND  ]] && \
+echo $GPG_PRIVATE_SILVERHAND > private.key && \
+gpg --import private.key
